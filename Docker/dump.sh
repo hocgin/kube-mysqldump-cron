308312c38179361400611da5054e7bfe721d86e0
hocgin <hocgin@gmail.com>
2022-12-28 21:09:43 +0800

    [新增功能](master): 指定默认字符集，防止 emoji 乱码

--- Docker/dump.sh
+++ Docker/dump.sh
@@ -34,13 +34,13 @@ if [[ ${ALL_DATABASES} == "" ]]; then
 		echo "Missing DB_NAME env variable"
 		exit 1
 	fi
-	mysqldump --user="${DB_USER}" --password="${DB_PASS}" --host="${DB_HOST}" "$@" "${DB_NAME}" > "${MYSQLDUMP}"/"${DB_NAME}".sql
+	mysqldump --default-character-set=utf8mb4 --user="${DB_USER}" --password="${DB_PASS}" --host="${DB_HOST}" "$@" "${DB_NAME}" > "${MYSQLDUMP}"/"${DB_NAME}".sql
 else
 	databases=`mysql --user="${DB_USER}" --password="${DB_PASS}" --host="${DB_HOST}" -e "SHOW DATABASES;" | tr -d "| " | grep -v Database`
   for db in $databases; do
       if [[ "$db" != "information_schema" ]] && [[ "$db" != "performance_schema" ]] && [[ "$db" != "mysql" ]] && [[ "$db" != _* ]] && [[ "$db" != "$IGNORE_DATABASE" ]]; then
           echo "Dumping database: $db"
-          mysqldump --user="${DB_USER}" --password="${DB_PASS}" --host="${DB_HOST}" --databases $db > "${MYSQLDUMP}"/$db.sql
+          mysqldump --default-character-set=utf8mb4 --user="${DB_USER}" --password="${DB_PASS}" --host="${DB_HOST}" --databases $db > "${MYSQLDUMP}"/$db.sql
       fi
   done
 fi
